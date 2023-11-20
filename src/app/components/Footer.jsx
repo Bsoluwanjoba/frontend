f93979367a547174220543f2f1ab5f0ef9ee8f10
Bsoluwanjoba <bsoluwanjoba@gmail.com>
2023-11-20 18:41:04 +0100

    modify footer files

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-     <footer className="bg-gold flex items-center justify-center h-[20vh]">
+     <footer className="bg-[gold] flex items-center justify-center h-[20vh]">
         <p>&copy; univelcity {new Date().getFullYear()}</p>
      </footer>
   )
